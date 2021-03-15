5452747ddd81334a664585bbef7e3a1e547c0385
hidmi6 <78271440+hidmi6@users.noreply.github.com>
2021-03-15 11:48:06 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import Product from './Products/Products';
 import Home from './Home/Home';
 import Orders from './Orders/Orders'
+import OrderDetail from './OrderDetails/OrderDetails'
 
 
 
@@ -59,6 +60,9 @@ function App() {
             </Route>
             <Route path="/orders">
               <Orders />
+            </Route>
+            <Route path = "/order/:id">
+               <OrderDetail />
             </Route>
           </Switch>
         </main>
